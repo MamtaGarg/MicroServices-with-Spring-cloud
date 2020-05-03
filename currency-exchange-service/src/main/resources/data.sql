e179797aecc3bfff1eb4e217d69e3b3066e9ba62
Mamta Garg <mamta_58@optum.com>
2020-05-03 13:44:56 +0530

    Initial commit

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+Insert into exchange_value(id, curreny_from, curreny_to, conversion_multiple, port)
+values(10001, 'USD', 'INR', 65, 0);
+Insert into exchange_value(id, curreny_from, curreny_to, conversion_multiple, port)
+values(10002, 'EUR', 'INR', 75, 0);
+Insert into exchange_value(id, curreny_from, curreny_to, conversion_multiple, port)
+values(10003, 'AUD', 'INR', 25, 0);
