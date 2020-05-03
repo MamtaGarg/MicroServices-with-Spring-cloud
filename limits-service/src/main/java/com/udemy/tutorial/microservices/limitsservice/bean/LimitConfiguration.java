7c177c4fbc9745434da72802d6d8357bd8e96762
Mamta Garg <mamta_58@optum.com>
2020-05-03 14:02:53 +0530

    Initial commit

---
+++ limits-service/src/main/java/com/udemy/tutorial/microservices/limitsservice/bean/LimitConfiguration.java
@@ -0,0 +1,27 @@
+package com.udemy.tutorial.microservices.limitsservice.bean;
+
+public class LimitConfiguration {
+
+	private int maximum;
+
+	private int minimum;
+
+	protected LimitConfiguration() {
+
+	}
+
+	public LimitConfiguration(int maximum, int minimum) {
+		super();
+		this.maximum = maximum;
+		this.minimum = minimum;
+	}
+
+	public int getMaximum() {
+		return maximum;
+	}
+
+	public int getMinimum() {
+		return minimum;
+	}
+
+}
