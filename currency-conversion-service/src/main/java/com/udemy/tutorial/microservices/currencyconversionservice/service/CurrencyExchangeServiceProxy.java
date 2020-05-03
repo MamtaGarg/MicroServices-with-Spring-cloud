c49bad7db27a20cfe5989aa5db4edd3a015e0b0d
Mamta Garg <mamta_58@optum.com>
2020-05-03 15:05:14 +0530

    Load balancing using Ribbon - conversion server can send request on any of two instances of exchange server

--- currency-conversion-service/src/main/java/com/udemy/tutorial/microservices/currencyconversionservice/service/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/com/udemy/tutorial/microservices/currencyconversionservice/service/CurrencyExchangeServiceProxy.java
@@ -1,12 +1,15 @@
 package com.udemy.tutorial.microservices.currencyconversionservice.service;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.udemy.tutorial.microservices.currencyconversionservice.bean.CurrencyConversionBean;
 
-@FeignClient(name="currency-exchange-service", url="localhost:8000")
+//@FeignClient(name="currency-exchange-service", url="localhost:8000")
+@FeignClient(name="currency-exchange-service") // used to call currency-exchange-service
+@RibbonClient(name="currency-exchange-service") // used for load balancing, can call any instance of currency-exchange-service
 public interface CurrencyExchangeServiceProxy {
 
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
