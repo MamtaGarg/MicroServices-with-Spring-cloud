82b414b963a0bed2e1dc843aba74c68f03340416
Mamta Garg <mamta_58@optum.com>
2020-05-05 13:58:54 +0530

    call currency-exchange-service through netflix Zuul Api gateway

--- currency-conversion-service/src/main/java/com/udemy/tutorial/microservices/currencyconversionservice/service/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/com/udemy/tutorial/microservices/currencyconversionservice/service/CurrencyExchangeServiceProxy.java
@@ -8,11 +8,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.udemy.tutorial.microservices.currencyconversionservice.bean.CurrencyConversionBean;
 
 //@FeignClient(name="currency-exchange-service", url="localhost:8000")
-@FeignClient(name="currency-exchange-service") // used to call currency-exchange-service
+//@FeignClient(name="currency-exchange-service") // used to call currency-exchange-service
+@FeignClient(name="netflix-zuul-api-gateway-server") // used to call currency-exchange-server through API Gateway
 @RibbonClient(name="currency-exchange-service") // used for load balancing, can call any instance of currency-exchange-service
 public interface CurrencyExchangeServiceProxy {
 
-	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	//@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	@GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")
 	public CurrencyConversionBean retrieveExchangeValue(@PathVariable String from, @PathVariable String to);
 	
 }
